5f5a7a4d29a95c7cdc032dfe96fa39bda11f5d44
bjakushka <bjakushka@raccoon>
2016-10-29 21:21:18 +0300

    Initial commit

---
+++ rc.lua
@@ -0,0 +1,16 @@
+require("termit.colormaps")
+
+defaults = {}
+defaults.windowTitle = 'Termit'
+defaults.allowChangingTitle = false
+defaults.tabName = 'Terminal'
+defaults.font = 'Consolas 16'
+defaults.hideSingleTab = true
+defaults.hideMenubar = true
+defaults.backgroundColor = 'rgba(66, 66, 66, 0.8)'
+defaults.transparentBackground = true
+defaults.transparentSaturation = 0.8
+defaults.colormap = termit.colormaps.delicate
+
+
+setOptions(defaults)
